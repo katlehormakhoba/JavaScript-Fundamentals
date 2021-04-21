e98d779668f6bbe7e12d2766064418db408a6e14
katleho R. Makhoba <katlehormakhoba@gmail.com>
2021-04-21 04:52:55 +0200

    first commit

---
+++ notes.js
@@ -0,0 +1,19 @@
+const age= 17;
+const isOldEnough = age >= 18;
+
+if(isOldEnough){
+    console.log('Sara can start driving licence 🚗');
+}else{
+    yearsLeft = 18 - age;
+    console.log(`Sara is too young. Wait another ${yearsLeft}`);
+}
+
+
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+console.log(18 =='18');
+
+const fav = prompt(`Whats your favorate number ?`);
+
+if(fav == 23) console.log(`Cool! 23 is an amazing number.😎`);
---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can drive😋');
+
+
+//FUNCTIONS
+
+function logger() {
+    console.log(`My name is Katleho`);
+}
+
+
+logger();
+
+
+function fruitProcessor(apples, oranges){
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+
+
+    return juice;
+}
+
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/katlehormakhoba/JavaScript-Fundamentals.git
+git push -u origin main
