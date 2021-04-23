20ad5a344f471f8bcacf4456c173c48c5f1d3401
katleho R. Makhoba <katlehormakhoba@gmail.com>
2021-04-23 16:03:19 +0200

    Objects

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 'use strict';
 
 
-// ARRAY
+//OBJECT
+
+const jonas = {
+    firstName: 'Katleho',
+    lastName: 'Makhoba',
+    age: 2021-1996,
+    job: 'Software Developer',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+
+console.log(jonas[`first${nameKey}`]);
+console.log(jonas['last'+nameKey]);
 
 
+const interestedIn = prompt(`What do you want to know about Katleho ? 
+                    Choose between firstName, lastName, job, age`);
 
-
-const friends = ['Michael', 'Steven', 'Peter'];
-
-const years = new Array(1991, 1992, 1983, 1985);
-
-//ADDING ELEMENTS ON ARRAY
-console.log(friends.push('Jay')); // last
-console.log(friends.unshift('John')); // first
-
-console.log(friends);
-
-//REMOVING ELEMENTS ON ARRAY
-friends.pop(); // last
-const popped = friends.pop();
-console.log(popped);
-console.log(friends);
-
-friends.shift(); //first
-console.log(friends);
-
-console.log(friends.indexOf('Steven'));
-
-console.log(friends.indexOf('Bob'));
-
-
-console.log(friends.includes('Bob'));
-console.log(friends.includes('Steven'));
+console.log(jonas[interestedIn]);
