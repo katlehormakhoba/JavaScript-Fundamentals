661879cca7cc9f61073dcab375182294d4b3d4d7
katleho R. Makhoba <katlehormakhoba@gmail.com>
2021-05-09 10:15:25 +0200

    guess number complete

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -6,31 +6,44 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highScore = 0;
+document.querySelector('.number').textContent = '?';
 
 document.querySelector('.check').addEventListener('click', function () {
-  // document.querySelector('.message').textContent = 'Correct Number🎉';
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
+
   if (!guess) {
     document.querySelector('.message').textContent = '⛔No number';
   } else if (guess === secretNumber) {
-
     document.querySelector('.message').textContent = 'Correct Number🎉';
-    
-  } else if (guess > secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
 
+    if (score > highScore) highScore = score;
+
+    document.querySelector('.highscore').textContent = highScore;
+  } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = 'Too High😔';
     score--;
     document.querySelector('.score').textContent = score;
-
   } else if (guess < secretNumber) {
-
     document.querySelector('.message').textContent = 'Too Low😔';
     score--;
     document.querySelector('.score').textContent = score;
-
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '30rem';
+  document.querySelector('.score').textContent = score;
+  score = 20;
 });
